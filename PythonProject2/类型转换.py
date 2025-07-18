635c9db30025ed42c74bbd96e874e2c48986f784
thisisbaron <thisisbaronsun@.com>
2025-07-18 10:52:55 +0800

    Changes

---
+++ PythonProject2/类型转换.py
@@ -0,0 +1,39 @@
+a = 333
+b = str(a)
+print(b)
+print(type(b))
+
+c = 3.14
+d = str(c)
+print(d)
+print(type(d))
+
+e = 333
+f = float(e)
+print(f)
+print(type(f))
+
+g = 3.64
+h = int(g)
+print(h)
+print(type(h))
+
+i = '333'
+j = int(i)
+print(j)
+print(type(j))
+
+#  + 表示两个元素相加
+print('+ : 2 + 3的值是:', 2 + 3) # 5
+#  - 表示两个元素相减
+print('- : 3 - 2的值是:', 3 - 2) # 1
+#  * 表示两个元素相乘
+print('* : 2 * 3的值是:', 2 * 3) # 6
+#  / 表示两个元素相除
+print('+ : 4 / 2的值是:', 4 / 2) # 2
+#  // 表示两个数字取整除
+print('// : 9 // 2的值是:', 9 // 2) # 4 .... 1
+# % 数字之间取余数
+print('% : 9 % 2的值是：' ,9 % 2) # 4 .... 1
+# **指数
+print("**: 2 ** 4的值是:", 2 ** 4) # 16
