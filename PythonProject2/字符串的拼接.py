708ab2cf1cd9279cab4a071a301f569a21d42758
thisisbaron <thisisbaronsun@.com>
2025-07-18 17:27:50 +0800

    第一次

---
+++ PythonProject2/字符串的拼接.py
@@ -0,0 +1,6 @@
+name = "李四"
+hobby = "跑步"
+age = 19
+gender = "男"
+
+print("我的名字叫："+ name + "我的年龄是：" + str(age) + "我的性别是:" + gender + "我的爱好："+ hobby )
---
+++ PythonProject2/字符串的格式化.py
@@ -0,0 +1,16 @@
+name = '张三'
+
+text = "我的名字是%s." %name
+print(text)
+age = 20
+text1 = "我的名字叫，今年"
+
+class_num = 5
+text2 = "我是高一%5d班的学生." % class_num
+print(text2)
+
+score = 88.9
+text3 = "我的考试分数是:%.2f" % score
+print(text3)
+text4 = "我的考试分数是:%.6f" % score
+print(text4)
